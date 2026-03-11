0b0d11ccb024a43b98526885b2cd044794388fb6
IonCIG <comanescuionutz@yahoo.com>
2026-03-11 17:42:05 +0200

    Laborator 1 - Inteligenta Artificiala

---
+++ Lab1.py
@@ -0,0 +1,33 @@
+print(type(1))
+print(10-3)
+print(10//3)
+print(round(10.567))
+print(pow(8,6))
+age=input("enter your age: ")
+age=int(age)
+print(type('hello'))
+print('hey you'[4])
+name='ionut'
+print(name[1])
+print(name[::-1])
+print('ionut'+'comanescu')
+print('ionut'*10)
+print(len('turtle'))
+print('i am alone'.strip())
+nume1 ='paul'
+nume2 ='ion'
+print(f'buna noi suntem {nume1} si {nume2}')
+print(bool(0))
+print(bool(3))
+lista=[2,2,4,'i',True]
+print(lista.count(2))
+print(lista[:1])
+dictionar = {'nume': 'ionut','age':21, 'inalt': False}
+print(dictionar['nume'])
+print(list(dictionar.keys()))
+tupla=('mar','para','banana')
+print(tupla[1])
+
+
+
+
